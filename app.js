a9383a50bf23c2eb3dc517c66936a2e30dc0fa4e
stumpfjo <stumpfjo@oregonstate.edu>
2021-12-01 16:00:05 -0500

    ui skeleton

--- app.js
+++ app.js
@@ -3,12 +3,34 @@ const bodyParser = require("body-parser");
 const https = require("https")
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res){
-    res.send("<h1>Hello world</h1>");
+    res.render("index.ejs");
 });
 
+app.get("/team", function(req, res) {
+    res.render("team.ejs");
+})
+
+app.get("/map", function(req, res) {
+    res.render("map.ejs");
+})
+
+app.get("/history", function(req, res) {
+    res.render("history.ejs");
+})
+
+app.get("/photos", function(req, res) {
+    res.render("photos.ejs");
+})
+
+app.get("/amenities", function(req, res) {
+    res.render("amenities.ejs");
+})
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
